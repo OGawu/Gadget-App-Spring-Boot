524db9f04bf2d39166cb02f58be68264721903a8
Obvious Gawu <obvious3011@gmail.com>
2024-11-21 14:18:26 +0200

    merge fix

--- src/main/java/com/example/gadget/GadgetConfig.java
+++ src/main/java/com/example/gadget/GadgetConfig.java
@@ -1,22 +1,33 @@
 package com.example.gadget;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.cglib.core.Local;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Locale;
 
 import static java.util.Calendar.FEBRUARY;
 import static java.util.Calendar.NOVEMBER;
 
+
+import static java.time.Month.*;
+@RestController
+
 @Configuration
 public class GadgetConfig {
+
 
     @Bean
     CommandLineRunner commandLineRunner(GadgetRepository repository){
         return args -> {
+
+
            Gadget iPhone = new Gadget(
                     "Iphone 15 Pro Max",
                     "Apple",
@@ -27,6 +38,7 @@ public class GadgetConfig {
                     LocalDate.of(2010,NOVEMBER,30)
 
             );
+
             Gadget MacBook = new Gadget(
                     "Macbook Pro-M3",
                     "Apple",
@@ -37,7 +49,32 @@ public class GadgetConfig {
                     LocalDate.of(2021,FEBRUARY,14)
 
             );
+
             repository.saveAll(List.of(iPhone, MacBook));
+
+            Gadget VisionPro = new Gadget(
+                    "Vision Pro",
+                    "Apple",
+                    "Space Black",
+                    "DBCG234RSG",
+                    "2WTUD/A",
+                    1234.98,
+                    LocalDate.of(2023,JUNE,16)
+            );
+            Gadget Desktop = new Gadget(
+                    "Desktop 1",
+                    "Dell Laptops",
+                    "Silver, 14 inch",
+                    "asdfghjklh",
+                    "qwer",
+                    1234.44,
+                    LocalDate.of(2023,NOVEMBER,16)
+
+            );
+
+            repository.saveAll(
+                    List.of(iPhone, MacBook,VisionPro, Desktop));
+
 
 
 
--- src/main/java/com/example/gadget/GadgetController.java
+++ src/main/java/com/example/gadget/GadgetController.java
@@ -1,10 +1,9 @@
 package com.example.gadget;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,15 +14,44 @@ public class GadgetController {
 
     @Autowired
     public GadgetController(GadgetService gadgetService) {
+
         this.gadgetService = gadgetService;
     }
 
     @GetMapping
-    public List<Gadget> getGadget(){
+    public List<Gadget> getGadgets(){
+
         return gadgetService.getGadget();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Gadget> getGadget(@PathVariable("id") Long  id) {
+
+        return ResponseEntity.ok(gadgetService.getGadgetById(id));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Gadget> registerNewGadget(@RequestBody Gadget gadget){
+
+        gadgetService.addNewGadget(gadget);
+        return ResponseEntity.status(HttpStatus.CREATED).body(gadget);
 
     }
 
+    @DeleteMapping(path = "{gadgetId}")
+    public void deleteGadget(@PathVariable("gadgetId") Long gadgetId){
+
+        gadgetService.deleteGadget(gadgetId);
+    }
+
+    @PutMapping(path = "{gadgetId}")
+    public void updateGadget(
+            @PathVariable("gadgetId") Long gadgetId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String serialNumber){
+        gadgetService.updateGadget(gadgetId, name, serialNumber);
+    }
 
 
 }
